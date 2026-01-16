f8bcc0d638bb9d4f735c0d0ca9d2106c7eef327c
islam-dja <islam-dja@users.noreply.github.com>
2026-01-16 23:38:31 +0100

    Repository: committing remaining workspace edits

---
+++ destinyKids-frontend/src/data/mockProducts.ts
@@ -0,0 +1,20 @@
+export const MOCK_BESTSELLERS = [
+  {
+    id: "mock-1",
+    name: "Casbah Doll",
+    price: 2500,
+    image: "/images/casbah-heritage-doll.png",
+  },
+  {
+    id: "mock-2",
+    name: "Traditional Bride Doll",
+    price: 3200,
+    image: "/images/traditional-bride-doll.png",
+  },
+  {
+    id: "mock-3",
+    name: "Kabyle Doll",
+    price: 2100,
+    image: "/images/kabyle-doll.png",
+  },
+];
